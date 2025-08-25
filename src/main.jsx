68aa39ca894104dbd5b3737af48ca1808912c5b9
Jun-ccc <xuanjun0110@gmail.com>
2025-08-25 11:53:28 +0800

    0825-Am

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom'
 // import App from './App0805-1'
 import './sass/all.scss'
 // import App from './MyApp'
-import App from './pages/App0820'
+import App from './pages/App0825=localstorage2'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
